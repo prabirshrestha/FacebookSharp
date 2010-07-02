77119ce4dec4a547d49c471db78fbf4ce09d991f
Prabir Shrestha <prabir@softverk.is>
2010-07-02 17:03:45 +0700

    + GetObjects FacebookExtension

--- src/FacebookSharp/FacebookExtensions.cs
+++ src/FacebookSharp/FacebookExtensions.cs
@@ -10,6 +10,22 @@ namespace FacebookSharp
             return facebook.Request(id, parameters);
         }
 
-       
+        public static string GetObjects(this Facebook facebook, IDictionary<string, string> parameters, params  string[] ids)
+        {
+            StringBuilder joinedIds = new StringBuilder();
+            for (int i = 0; i < ids.Length; i++)
+            {
+                if (i == 0)
+                    joinedIds.Append(ids[i]);
+                else
+                    joinedIds.AppendFormat(",{0}", ids[i]);
+            }
+
+            if (parameters == null)
+                parameters = new Dictionary<string, string>();
+            parameters["ids"] = joinedIds.ToString();
+
+            return facebook.Request(null, parameters);
+        }
     }
 }
