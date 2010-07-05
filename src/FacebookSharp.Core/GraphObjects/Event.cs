f58195e481c6211b2bf3a4af1fb597dcbbd6b40c
Prabir Shrestha <prabir@softverk.is>
2010-07-05 19:14:35 +0700

    + Privacy enum + Events comments

--- src/FacebookSharp.Core/GraphObjects/Event.cs
+++ src/FacebookSharp.Core/GraphObjects/Event.cs
@@ -1,20 +1,45 @@
 namespace FacebookSharp
 {
+    /// <summary>
+    /// Represents the Facebook Event Graph API type.
+    /// </summary>
     public class Event : GraphObject
     {
+        /// <summary>
+        /// Gets or sets the name and ID of the user who owns the event.
+        /// </summary>
         public EventOwner Owner { get; set; }
+
         public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the long-form HTML description of the event.
+        /// </summary>
         public string Description { get; set; }
+        /// <summary>
+        /// Gets or sets the start time of the event.
+        /// </summary>
         public string Start_Time { get; set; }
+        /// <summary>
+        /// Gets or sets the end time of the event.
+        /// </summary>
         public string End_Time { get; set; }
+        /// <summary>
+        /// Gets or sets the last time the event was updated.
+        /// </summary>
         public string Updated_Time { get; set; }
+        /// <summary>
+        /// Gets or sets the location of this event.
+        /// </summary>
         public string Location { get; set; }
+        /// <summary>
+        /// Gets or sets the visibility of this event. Can be 'OPEN', 'CLOSED', or 'SECRET'.
+        /// </summary>
         public string Privacy { get; set; }
+        /// <summary>
+        /// Gets or sets the location of this event.
+        /// </summary>
         public Location Venue { get; set; }
     }
 
-    public class EventCollection : Connection<Event>
-    {
-
-    }
+    public class EventCollection : Connection<Event> { }
 }
---
+++ src/FacebookSharp.Core/GraphObjects/Privacy.cs
@@ -0,0 +1,9 @@
+namespace FacebookSharp
+{
+    public enum Privacy
+    {
+        Open,
+        Closed,
+        Secret
+    }
+}
