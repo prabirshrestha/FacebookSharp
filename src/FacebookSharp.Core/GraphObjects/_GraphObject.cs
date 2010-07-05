3953d20925fd346e0ceed2cc1d9f9d2f9ad84b0a
Prabir Shrestha <prabir@softverk.is>
2010-07-05 18:57:12 +0700

    + GraphObject comments

--- src/FacebookSharp.Core/GraphObjects/_GraphObject.cs
+++ src/FacebookSharp.Core/GraphObjects/_GraphObject.cs
@@ -1,10 +1,24 @@
 
 namespace FacebookSharp
 {
+    /// <summary>
+    /// Base class which encapsulates behavior and properties common to most Facebook Graph API types.
+    /// </summary>
     public abstract class GraphObject
     {
+        /// <summary>
+        /// Gets or sets the object's unique Facebook ID.
+        /// </summary>
         public string ID { get; set; }
 
+        /// <summary>
+        /// Gets or sets the object's type metadata, available by including metadata=1 URL parameter during request.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets metadata, available by including the metadata=1 URL parameter during request.
+        /// </summary>
         public Metadata Metadata { get; set; }
 
         public Paging Paging { get; set; }
