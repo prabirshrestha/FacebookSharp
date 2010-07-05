2a43cb0e6da5e4cb72e090e96fbad8146abfc421
Prabir Shrestha <prabir@softverk.is>
2010-07-05 18:45:51 +0700

    schemas is generated under FacebookSharp.Schemas.Rest namespace

--- src/FacebookSharp.Core/Schemas/generate_schemas.bat
+++ src/FacebookSharp.Core/Schemas/generate_schemas.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files (x86)\Xsd2Code\Xsd2Code" facebook.xsd FacebookSharp.Schemas schemas.cs
+"C:\Program Files (x86)\Xsd2Code\Xsd2Code" facebook.xsd FacebookSharp.Schemas.Rest schemas.cs
 @pause
