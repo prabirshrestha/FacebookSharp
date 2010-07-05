1989e833b6639b8d346e249ae5404688db28b38e
Prabir Shrestha <prabir@softverk.is>
2010-07-05 16:51:23 +0700

    + Feed, NamedGraphObject, Friend

---
+++ src/FacebookSharp.Core/GraphObjects/Feed.cs
@@ -0,0 +1,33 @@
+
+namespace FacebookSharp
+{
+    public class Feed : GraphObject
+    {
+        public Friend Author { get; set; }
+
+        public string Message { get; set; }
+
+        public string Picture { get; set; }
+
+        public string Link { get; set; }
+
+        public string Name { get; set; }
+
+        public string Caption { get; set; }
+
+        public string Description { get; set; }
+
+        public string Source { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Attribution { get; set; }
+
+        public long Likes { get; set; }
+
+        public string Created_Time { get; set; }
+
+        public string Updated_Time { get; set; }
+
+    }
+}
---
+++ src/FacebookSharp.Core/GraphObjects/_NamedGraphObject.cs
@@ -0,0 +1,7 @@
+namespace FacebookSharp
+{
+    public abstract class NamedGraphObject : GraphObject
+    {
+        public string Name { get; set; }
+    }
+}
