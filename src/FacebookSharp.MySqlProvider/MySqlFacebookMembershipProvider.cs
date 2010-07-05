0a87eff5a2c8359eae503524a05140b7dbb59d30
Prabir Shrestha <prabir@softverk.is>
2010-07-05 12:07:54 +0700

    + MySql project for IFacebookMembershipProvider

---
+++ src/FacebookSharp.MySqlProvider/MySqlFacebookMembershipProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace FacebookSharp.MySqlProvider
+{
+    public class MySqlFacebookMembershipProvider : IFacebookMembershipProvider
+    {
+        #region Implementation of IFacebookMembershipProvider
+
+        public bool HasLinkedFacebook(string membershipUsername)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasLinkedFacebook(object membershipProviderUserKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFacebookUserLinked(string facebookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LinkFacebook(string membershipUsername, string facebookId, string accessToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LinkFacebook(object membershipProviderUserKey, string facebookId, string accessToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnlinkFacebook(string membershipUsername)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnlinkFacebook(object membershipProviderUserKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnlinkFacebookByFacebookId(string facebookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFacebookAccessToken(string membershipUsername)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFacebookAccessToken(object membershipProviderUserKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFacebookAccessTokenByFacebookId(string facebookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFacebookId(string membershipUsername)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFacebookId(object membershipProviderUserKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
