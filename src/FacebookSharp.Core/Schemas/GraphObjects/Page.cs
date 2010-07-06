6cbeedc52a40f2f8b973c1086414f6911e7e314e
Prabir Shrestha <prabir@softverk.is>
2010-07-06 11:00:31 +0700

    + Page

---
+++ src/FacebookSharp.Core/Schemas/GraphObjects/Page.cs
@@ -0,0 +1,60 @@
+using System.Runtime.Serialization;
+
+namespace FacebookSharp.Schemas.Graph
+{
+    /// <summary>
+    /// Represetns the Facebook Page Graph API type.
+    /// </summary>
+    [DataContract]
+    public class Page : CategorizedGraphObject
+    {
+        /// <summary>
+        /// Gets or sets the page's picture.
+        /// </summary>
+        [DataMember(Name = "picture")]
+        public string Picture { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page's link.
+        /// </summary>
+        [DataMember(Name = "link")]
+        public string Link { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page's username.
+        /// </summary>
+        [DataMember(Name = "username")]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// gets or sets when the page was founded.
+        /// </summary>
+        [DataMember(Name = "founded")]
+        public string Founded { get; set; }
+
+        /// <summary>
+        /// Gets or sets the overview of the page's company.
+        /// </summary>
+        [DataMember(Name = "company_overview")]
+        public string CompanyOverview { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page's mission.
+        /// </summary>
+        [DataMember(Name = "mission")]
+        public string Mission { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page's products.
+        /// </summary>
+        [DataMember(Name = "products")]
+        public string Products { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of fans the page has.
+        /// </summary>
+        [DataMember(Name = "fan_count")]
+        public long FanCount { get; set; }
+
+    }
+}
