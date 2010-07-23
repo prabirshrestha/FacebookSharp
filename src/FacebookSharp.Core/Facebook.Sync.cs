20accd6344f4ecdac50daf2e7cb7d23610063343
Prabir Shrestha <mail@prabir.me>
2010-07-23 21:52:05 +0700

    + addAccessToken param for Execute helpers

--- src/FacebookSharp.Core/Facebook.Sync.cs
+++ src/FacebookSharp.Core/Facebook.Sync.cs
@@ -1,22 +1,29 @@
 namespace FacebookSharp
 {
     using RestSharp;
+    using RestSharp.Authenticators;
 
     public partial class Facebook
     {
 #if !SILVERLIGHT
-        private RestResponse Execute(RestRequest request)
+        private RestResponse Execute(RestRequest request, bool addAccessToken)
         {
-            var client = new RestClient();
-            client.BaseUrl = GraphBaseUrl;
+            var client = new RestClient(GraphBaseUrl);
+
+            if (addAccessToken && !string.IsNullOrEmpty(Settings.AccessToken))
+                client.Authenticator = new OAuth2UriQueryParamaterAuthenticator(Settings.AccessToken);
+
             return client.Execute(request);
         }
 
-        private RestResponse<T> Execute<T>(RestRequest request)
+        private RestResponse<T> Execute<T>(RestRequest request, bool addAccessToken)
             where T : new()
         {
-            var client = new RestClient();
-            client.BaseUrl = GraphBaseUrl;
+            var client = new RestClient(GraphBaseUrl);
+
+            if (addAccessToken && !string.IsNullOrEmpty(Settings.AccessToken))
+                client.Authenticator = new OAuth2UriQueryParamaterAuthenticator(Settings.AccessToken);
+
             return client.Execute<T>(request);
         }
 #endif
