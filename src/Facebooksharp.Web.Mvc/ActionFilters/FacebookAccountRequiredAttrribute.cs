12986687adacd56f684014368eb7b2d63c8a3ded
Prabir Shrestha <mail@prabir.me>
2010-07-24 18:04:39 +0700

    + empty FacebookAccountRequired action filter

---
+++ src/Facebooksharp.Web.Mvc/ActionFilters/FacebookAccountRequiredAttrribute.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Web.Mvc;
+
+namespace FacebookSharp.Web.Mvc
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class FacebookAccountRequiredAttribute : AuthorizeAttribute
+    {
+    }
+}
