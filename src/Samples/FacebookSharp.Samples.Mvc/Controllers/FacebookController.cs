8310fe9bc5b73c512e78ded9e32f5a3b55661856
Prabir Shrestha <mail@prabir.me>
2010-07-24 17:40:33 +0700

    + rename FacebookContoller to BaseFacebookContoller

---
+++ src/Samples/FacebookSharp.Samples.Mvc/Controllers/FacebookController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FacebookSharp.Samples.Mvc.Controllers
+{
+    // you can dervie from BaseFacebookController which will make your life easier
+    // but since most of u will already have another base controller i will not
+    // derive from BaseFacebookController
+    public class FacebookController : Controller
+    {
+        //
+        // GET: /Facebook/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
