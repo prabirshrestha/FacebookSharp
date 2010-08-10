44a23f40efe28a66ab47f583513027e3c95123d8
prabirshrestha <mail@prabir.me>
2010-08-10 11:07:02 +0700

    + updated Restsharp libary * OAuth2UriQueryParameterAuthenticator spelling mistake

--- src/FacebookSharp.Core/Facebook.Sync.cs
+++ src/FacebookSharp.Core/Facebook.Sync.cs
@@ -11,8 +11,8 @@ namespace FacebookSharp
             var client = new RestClient(GraphBaseUrl);
 
             if (addAccessToken && !string.IsNullOrEmpty(Settings.AccessToken)) // todo: check if acces_token already added.
-                client.Authenticator = new OAuth2UriQueryParamaterAuthenticator(Settings.AccessToken);
-
+                client.Authenticator = new OAuth2UriQueryParameterAuthenticator(Settings.AccessToken);
+                                           
             return client.Execute(request);
         }
 #endif  
