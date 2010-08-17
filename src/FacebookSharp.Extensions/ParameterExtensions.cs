ca146be3b180e39c76565beb546e64f26cd2ba62
prabirshrestha <mail@prabir.me>
2010-08-17 17:46:53 +0700

    + SelectFields parametter extensions

--- src/FacebookSharp.Extensions/ParameterExtensions.cs
+++ src/FacebookSharp.Extensions/ParameterExtensions.cs
@@ -1,6 +1,7 @@
 namespace FacebookSharp.Extensions
 {
     using System.Collections.Generic;
+    using System.Text;
 
     public static partial class FacebookExtensions
     {
@@ -48,6 +49,59 @@ namespace FacebookSharp.Extensions
                 parameters = new Dictionary<string, string>();
             parameters.Add("until", until);
             return parameters;
+        }
+
+        /// <summary>
+        /// Add's fields parameter, if fields already exisits appends it.
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <param name="fieldNames">The field names.</param>
+        /// <returns>
+        /// </returns>
+        public static IDictionary<string, string> SelectFields(this IDictionary<string, string> parameters, string[] fieldNames)
+        {
+            if (fieldNames == null || fieldNames.Length == 0) // don't do anything
+                return parameters;
+
+            if (parameters == null)
+                parameters = new Dictionary<string, string>();
+
+            StringBuilder oldFields;
+
+            if (parameters.ContainsKey("fields"))
+            {
+                oldFields = new StringBuilder(parameters["fields"]);
+                parameters.Remove("fields");
+            }
+            else
+                oldFields = new StringBuilder();
+
+            if (oldFields.Length > 0)
+                oldFields.Append(',');
+
+            foreach (var field in fieldNames)
+            {
+                oldFields.Append(field);
+                oldFields.Append(',');
+            }
+
+            oldFields.Remove(oldFields.Length - 1, 1); // remove the last comma
+
+            parameters.Add("fields", oldFields.ToString());
+
+            return parameters;
+        }
+
+        /// <summary>
+        /// Add's fields parameter, if fields already exisits appends it.
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <param name="fieldName">The field name.</param>
+        /// <returns>
+        /// </returns>
+        public static IDictionary<string, string> SelectField(this IDictionary<string, string> parameters, string fieldName)
+        {
+            return parameters.SelectFields(new[] { fieldName });
         }
     }
 }
