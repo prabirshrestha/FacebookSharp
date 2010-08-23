a9890a467abe9f21c1d343728c85555b7b1b8330
prabirshrestha <mail@prabir.me>
2010-08-23 10:50:00 +0700

    + P3PHeaderAttribute action filter * P3PHeaderAttribute in CanvasIFrameApplication (issue #6)

--- src/Samples/FacebookSharp.Samples.CanvasIFrameApplication/Controllers/HomeController.cs
+++ src/Samples/FacebookSharp.Samples.CanvasIFrameApplication/Controllers/HomeController.cs
@@ -9,11 +9,52 @@ using System.Web.Mvc;
 using System.Web.Security;
 using FacebookSharp.Extensions;
 using FacebookSharp.Schemas.Graph;
+using FacebookSharp.Web.Mvc;
 
 namespace FacebookSharp.Samples.CanvasIFrameApplication.Controllers
 {
+    [P3PHeader]
     public class HomeController : Controller, IFacebookContext
     {
+
+        /* Note: iframe Applications and P3P HTTP Headers
+         *  references: http://github.com/prabirshrestha/FacebookSharp/issues#issue/6
+         *              http://forum.developers.facebook.net/viewtopic.php?id=452
+         *              
+         * incase you are using iframe application, make sure you include P3P HTTP headers
+         * 
+         * Q: Why "make sure to implement P3P if using iframes" ?
+         * A: If your application is inside iframe with parent belongs to another domain - cookies will not work for some very common configurations for example IE 6/7 with privacy set to medium. If cookies don't work - session won't work.
+         * Therefore session state turns out useless for your application under Internet Explorer. See - Privacy in Internet Explorer 6.
+         * Solution - need to implement P3P header to tell the browser that cookies for your application inside iframe are OK for user privacy.
+         * So, ASP.NET implementation may look like the following (global.asax):
+         * 
+         * protected void Application_BeginRequest(Object sender, EventArgs e)
+         * {
+         *      HttpContext.Current.Response.AddHeader("p3p", "CP=\"CAO PSA OUR\"");
+         * }
+         * 
+         * or in asp.net website .aspx page code behind.
+         * 
+         * protected override void OnPreRender(EventArgs e) 
+         * {
+         *      Response.AppendHeader("P3P", "CP=\"CAO PSA OUR\"");
+         *      base.OnPreRender(e);
+         * }
+         * 
+         * or in MVC for every controllers you can manually add P3P header
+         * 
+         * public ActionResult Index()
+         * {
+         *      Response.AppendHeader("P3P", "CP=\"CAO PSA OUR\"");
+         *      // write your code here
+         * }
+         * 
+         * to make it easier, FacebookSharp already contains an action filter called P3P header action filter
+         * [P3PHeader]
+         * 
+         */
+
         #region Implementation of IFacebookContext
 
         private IFacebookMembershipProvider _facebookMembershipProvider;
@@ -80,6 +121,8 @@ namespace FacebookSharp.Samples.CanvasIFrameApplication.Controllers
         public ActionResult Index()
         {
             // note: need to enable Canvas Session Parameter and OAuth 2.0 for Canvas (beta) in Migration Tab in app settings.
+
+            
 
             if (FacebookContext.IsSessionValid())
                 ViewData["name"] = FacebookContext.Get<User>("/me").Name;
