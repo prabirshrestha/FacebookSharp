cc483b9df74cdc56ed01c74dd270c988cb769e0c
prabirshrestha <mail@prabir.me>
2010-09-03 22:49:37 +0700

    + clean rakefile task

--- rakefile.rb
+++ rakefile.rb
@@ -1,10 +1,23 @@
 require 'albacore'
 
-task :default => :build
+CONFIG		= "Release"
+
+SRC_PATH	= "src/"
+LIBS_PATH	= "libs/"
+OUTPUT_PATH = "bin/"
+
+task :default => :full
+
+task :full => [:clean, :build_release]
+
+desc "Clean"
+task :clean do
+	FileUtils.rm_rf OUTPUT_PATH
+end
 
 desc "Build solution using MSBuild"
-msbuild :build do |msb|
-	msb.solution = "src/FacebookSharp.sln"
-	msb.targets	:clean, :build
-	msb.properties :configuration => :release
+msbuild :build_release do |msb|
+	msb.properties :configuration => :release	
+	msb.solution = SRC_PATH + "FacebookSharp.sln"
+	msb.targets	:Build
 end
