23e1df45e4dce971bd267cf2d0d927c47f2eaa6b
prabirshrestha <mail@prabir.me>
2010-09-04 13:29:36 +0700

    * configuration to Release in rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,23 +1,23 @@
 require 'albacore'
 
-CONFIG		= "Release"
+CONFIGURATION = "Release"
 
-SRC_PATH	= "src/"
-LIBS_PATH	= "libs/"
-OUTPUT_PATH = "bin/"
+SRC_PATH		= "src/"
+LIBS_PATH		= "libs/"
+OUTPUT_PATH		= "bin/"
 
 task :default => :full
 
 task :full => [:build_release]
 
-desc "Clean"
+desc "Clean build outputs"
 task :clean do
 	FileUtils.rm_rf OUTPUT_PATH
 end
 
-desc "Build solution using MSBuild"
+desc "Build solution"
 msbuild :build_release => [:clean] do |msb|
-	msb.properties :configuration => :release	
+	msb.properties :configuration => :Release
 	msb.solution = SRC_PATH + "FacebookSharp.sln"
 	msb.targets	:Build
 end
