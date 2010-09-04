1b54d140a4027758c8ba97ff88f7d6629f5cfa97
prabirshrestha <mail@prabir.me>
2010-09-04 15:09:57 +0700

    + rake prepare to copy license and readme

--- rakefile.rb
+++ rakefile.rb
@@ -10,6 +10,13 @@ task :default => :full
 
 task :full => [:build_release]
 
+desc "Prepare build"
+task :prepare do
+	mkdir OUTPUT_PATH unless File.exists?(OUTPUT_PATH)
+	cp "LICENSE.txt", OUTPUT_PATH
+	cp "README.md" , OUTPUT_PATH
+end
+
 desc "Clean build outputs"
 task :clean => [:clean_msbuild] do
 	FileUtils.rm_rf OUTPUT_PATH
@@ -22,8 +29,8 @@ msbuild :clean_msbuild do |msb|
 	msb.targets	:Clean
 end
 
-desc "Build solution"
-msbuild :build_release => [:clean] do |msb|
+desc "Build solution (default)"
+msbuild :build_release => [:clean,:prepare] do |msb|
 	msb.properties :configuration => :Release
 	msb.solution = SRC_PATH + "FacebookSharp.sln"
 	msb.targets	:Build
