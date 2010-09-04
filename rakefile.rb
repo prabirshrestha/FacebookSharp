741c55211342a266dc50f8436de3d24b265c6dc9
prabirshrestha <mail@prabir.me>
2010-09-04 14:50:19 +0700

    + rakefile clean solution outputs

--- rakefile.rb
+++ rakefile.rb
@@ -11,8 +11,15 @@ task :default => :full
 task :full => [:build_release]
 
 desc "Clean build outputs"
-task :clean do
+task :clean => [:clean_msbuild] do
 	FileUtils.rm_rf OUTPUT_PATH
+end
+
+desc "Clean solution outputs"
+msbuild :clean_msbuild do |msb|
+	msb.properties :configuration => :Release
+	msb.solution = SRC_PATH + "FacebookSharp.sln"
+	msb.targets	:Clean
 end
 
 desc "Build solution"
