526d6f704b477b0f3f080f8217ed3d78eff75129
prabirshrestha <mail@prabir.me>
2010-09-05 22:29:09 +0700

    + SelectId unit tests

---
+++ src/Tests/FacebookSharp.Tests/Extensions/ParameterExtensions/SelectIdTests.cs
@@ -0,0 +1,68 @@
+namespace FacebookSharp.Tests.Extensions
+{
+    using System.Collections.Generic;
+    using FacebookSharp.Extensions;
+    using Xunit;
+
+    public class SelectIdTests
+    {
+        [Fact]
+        public void SelectId_InNullParameter()
+        {
+            IDictionary<string, string> parameters = null;
+
+            Assert.DoesNotThrow(
+                () =>
+                {
+                    parameters = parameters.SelectId("1");
+                });
+
+            Assert.Equal(1, parameters.Count);
+        }
+
+        [Fact]
+        public void SelectId_NewEmptyParameter()
+        {
+            IDictionary<string, string> parameters = new Dictionary<string, string>();
+
+            parameters = parameters.SelectId("1");
+
+            Assert.Equal(1, parameters.Count);
+
+            Assert.Equal("1", parameters["ids"]);
+        }
+
+        [Fact]
+        public void SelectId_AddToExisitingIdParameter()
+        {
+            IDictionary<string, string> parameters = new Dictionary<string, string> { { "ids", "1" } };
+
+            Assert.DoesNotThrow(
+                () =>
+                {
+                    parameters = parameters.SelectId("2");
+                });
+
+            Assert.Equal(1, parameters.Count);
+
+            Assert.Equal(3, parameters["ids"].Length);
+        }
+
+        [Fact]
+        public void SelectId_DoesNotEndInComma_WhenMoreParametersAdded()
+        {
+            IDictionary<string, string> parameters = new Dictionary<string, string>().SelectId("1");
+
+            // assert that it doesn't end with ,
+            Assert.NotEqual(',', parameters["ids"][parameters["ids"].Length - 1]);
+
+            parameters = parameters.SelectId("2");
+
+            // assert it contains , at 1st index -> 1,2
+            Assert.Equal(',', parameters["ids"][1]);
+
+            // assert that it doesn't end with ,
+            Assert.NotEqual(',', parameters["ids"][parameters["ids"].Length - 1]);
+        }
+    }
+}
