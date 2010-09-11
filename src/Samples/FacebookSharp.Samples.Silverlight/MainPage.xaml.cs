0a3ffe53ba3929e06cb6be4112ed5f25b1b13e24
prabirshrestha <mail@prabir.me>
2010-09-11 11:49:39 +0700

    + sample silverlight getasync

--- src/Samples/FacebookSharp.Samples.Silverlight/MainPage.xaml.cs
+++ src/Samples/FacebookSharp.Samples.Silverlight/MainPage.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using FacebookSharp;
+using FacebookSharp.Schemas.Graph;
 
 namespace FacebookSharp.Samples.Silverlight
 {
@@ -19,8 +21,43 @@ namespace FacebookSharp.Samples.Silverlight
             InitializeComponent();
         }
 
+        private void WriteLine(string str)
+        {
+            txtInfo.Dispatcher.BeginInvoke(
+                () =>
+                {
+                    txtInfo.Text += str + "\n";
+                });
+        }
+
         private void btnGetMyInfo_Click(object sender, RoutedEventArgs e)
         {
+            var fb = new Facebook(txtAccessToken.Text);
+
+            // using async method, silverlight supports only async methods
+            fb.GetAsync("/me",
+                result =>
+                {
+                    // incase you are using async,
+                    // always check if it was successful.
+                    if (result.IsSuccessful)
+                    {
+                        // this prints out the raw json
+                        WriteLine(result.RawResponse + "\n");
+
+                        // this mite be preferable - the generic version of the result
+                        var user = result.GetResponseAs<User>();
+                        WriteLine("Hi " + user.Name);
+                    }
+                    else
+                    {
+                        // exception is stored in result.Exception
+                        // u can extract the message using result.Exception.Message
+                        // or u can get raw facebook json exception using result.Response.
+                        WriteLine("Error: " + "\n" + result.Exception.Message + "\n");
+                    }
+                });
+
         }
 
     }
