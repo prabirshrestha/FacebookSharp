71b8ea6681e3b8fa7c394b0fea037f860407f5a7
anthonyvscode <azigenbine@gmail.com>
2010-10-06 09:31:06 +1000

    Significant Other is a type not a string, created a type for it and referenced it. Was causing an error when retrieving information.

---
+++ src/FacebookSharp.Core/Schemas/Graph/SignificantOther.cs
@@ -0,0 +1,11 @@
+﻿namespace FacebookSharp.Schemas.Graph
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class SignificantOther : NamedGraphObject
+    {
+        [DataMember(Name = "id")]
+        public string ID { get; set; }
+    }
+}
